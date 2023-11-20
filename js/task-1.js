0b806d48125d3c8112ba284222332f566f16f852
Rodion Matvieiev <matveev.r.v@icloud.com>
2023-11-19 22:17:00 -0500

    add

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,84 @@
-function slugify(title) {
-  return title.toLowerCase().split(" ").join("-");
+// function slugify(title) {
+//   return title.toLowerCase().split(" ").join("-");
+// }
+// console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+// console.log(slugify("English for developer")); // "english-for-developer"
+// console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+// console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+
+// const keys = [];
+// const values = [];
+// for (const key in apartment) {
+//   keys.push(key);
+// }
+// for (const value in apartment) {
+//   values.push(apartment[value]);
+// }
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+// const keys = Object.keys(apartment);
+// for (const key of keys) {
+//   console.log();
+// }
+
+// function calculateTotalPrice(productName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//   ];
+//   let totalPrice = 0;
+//   for (const product of products) {
+//     if (product.name === productName) {
+//       return (totalPrice = product.price * product.quantity);
+//     }
+//   }
+//   return `${productName} not found`;
+// }
+// console.log(calculateTotalPrice("Radar"));
+// console.log(calculateTotalPrice("Blaster"));
+// console.log(calculateTotalPrice("Droid"));
+
+// const atTheOldToad = {
+//   potions: [
+//     { name: "Speed potion", price: 460 },
+//     { name: "Stone skin", price: 520 },
+//   ],
+//   getPotions() {
+//     return this.potions;
+//   },
+//   addPotion(newPotion) {
+//     this.potions.push(newPotion);
+//   },
+//   getTotalPrice() {
+//     let totalPrice = 0;
+//     for (let potion of this.potions) {
+//       totalPrice += potion.price;
+//     }
+//     return totalPrice;
+//   },
+// };
+
+// console.log(atTheOldToad.getTotalPrice());
+function getExtremeScores(scores) {
+  const rate = {
+    best: Math.max(...scores),
+    worst: Math.min(...scores),
+  };
+  console.log(rate.best);
+  console.log(rate.worst);
 }
-console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+
+console.log(getExtremeScores([89, 64, 42, 17, 93, 51, 26]));
